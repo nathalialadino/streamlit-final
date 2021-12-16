7219db2954ae742bfa417ffc4eb850633ca26830
nathalialadino <91520264+nathalialadino@users.noreply.github.com>
2021-12-16 07:29:27 -0500

    Update streamlit507.py

--- streamlit507.py
+++ streamlit507.py
@@ -59,8 +59,9 @@ Hospital_data = ny_df[ny_df['hospital_name'] == select]
 st.header('New York Hospital Data')
 st.dataframe(Hospital_data)
 
-Hospital_data.style.set_properties(**{'border': '1.3px solid green',
-                          'color': 'magenta'})
+st.markdown('<style>.ReactVirtualized__Grid__innerScrollContainer div[class^="row"], .ReactVirtualized__Grid__innerScrollContainer div[class^="data row"]{ background:white; } </style>', unsafe_allow_html=True)
+
+
 st.subheader('Map of NY Hospital Locations')
 
 hospitals_ny_gps = ny_df['location'].str.strip('()').str.split(' ', expand=True).rename(columns={0: 'Point', 1:'lon', 2:'lat'}) 	

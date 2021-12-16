8b5bda26ddd8172970b63eae1f0fdf9e67d23f8f
nathalialadino <91520264+nathalialadino@users.noreply.github.com>
2021-12-16 02:04:23 -0500

    Update streamlit507.py

--- streamlit507.py
+++ streamlit507.py
@@ -51,9 +51,13 @@ select = st.sidebar.selectbox('Select a hospital',nyinpatient['provider_name'])
 #get the state selected in the selectbox
 state_data = nyinpatient[nyinpatient['provider_name'] == select]
 
-nyinpatient.loc[nyinpatient.provider_name != "UNIVERSITY HOSPITAL ( STONY BROOK )", "provider_name"] = "nonsb"
 
-fig = px.bar(nyinpatient, x="provider_name", y=["total_discharges", "average_covered_charges", "average_total_payments", "average_medicare_payments"], barmode='group', height=400)
+
+nyinpatient.loc[nyinpatient.provider_name != "UNIVERSITY HOSPITAL ( STONY BROOK )", "provider_name"] = "nonsb"
+inpatientcompare = nyinpatient.groupby("provider_name")["total_discharges", "average_covered_charges", "average_total_payments", "average_medicare_payments"].mean()
+
+
+fig = px.bar(nyinpatientcompare, x="provider_name", y=["total_discharges", "average_covered_charges", "average_total_payments", "average_medicare_payments"], barmode='group', height=400)
 # st.dataframe(df) # if need to display dataframe
 st.plotly_chart(fig)
 
